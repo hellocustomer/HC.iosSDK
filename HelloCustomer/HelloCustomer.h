242c41ef02d86c9cbbf103f93f71a5289838ecb3
= <piotr.bandurski@valueadd.pl>
2021-07-24 11:23:01 +0200

    Initial Commit

---
+++ HelloCustomer/HelloCustomer.h
@@ -0,0 +1,18 @@
+//
+//  HelloCustomer.h
+//  HelloCustomer
+//
+//  Created by Piotr Bandurski on 24/07/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloCustomer.
+FOUNDATION_EXPORT double HelloCustomerVersionNumber;
+
+//! Project version string for HelloCustomer.
+FOUNDATION_EXPORT const unsigned char HelloCustomerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloCustomer/PublicHeader.h>
+
+
